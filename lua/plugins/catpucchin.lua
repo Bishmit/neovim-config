49e41d03cd5b7803b0e6f31f9b50ce561571b467
Bishmit <bishmitregmi@gmail.com>
2025-02-17 22:05:44 +0545

    alacritty.toml added and opacity added

--- lua/plugins/catpucchin.lua
+++ lua/plugins/catpucchin.lua
@@ -1,11 +1,23 @@
 return {
-    {
-      "catppuccin/nvim",
-      lazy = false,
-      name = "catppuccin",
-      priority = 1000,
-      config = function()
-        vim.cmd.colorscheme "catppuccin-mocha"
-      end
-    }
+  {
+    "catppuccin/nvim",
+    lazy = false,
+    name = "catppuccin",
+    priority = 1000,
+    config = function()
+      require("catppuccin").setup({
+        transparent_background = true,  -- Enables transparency
+        integrations = {
+          telescope = true,
+          nvimtree = true,
+          treesitter = true,
+        }
+      })
+      vim.cmd.colorscheme "catppuccin-mocha"
+
+      -- Set terminal opacity (works best with a compositor like picom)
+      vim.api.nvim_set_hl(0, "Normal", { bg = "NONE" })
+      vim.api.nvim_set_hl(0, "NormalFloat", { bg = "NONE" })
+    end
   }
+}
